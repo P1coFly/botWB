c58a8ca901ed50572a4e0286d39d313063462300
Artem <artem3m@mail.ru>
2022-10-18 17:40:02 +0300

    initial bot

---
+++ bot/bot.py
@@ -0,0 +1,24 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+from config import TOKEN
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(msg: types.Message):
+    await bot.send_message(msg.from_user.id,"Привет!\nНапиши мне адресс!")
+
+    
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
+    
+
+if __name__ == '__main__':
+    print("Start work")
+    executor.start_polling(dp)
